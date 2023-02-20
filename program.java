ef2bcfdf7b8a72eb43b5d56c37d7f5eb28fc3ddd
Anton <anton98156789@gmail.com>
2023-02-20 10:07:27 +0300

    Done

--- program.java
+++ program.java
@@ -1,7 +1,33 @@
 // Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)
 
+// import java.util.Random;
+// import java.util.ArrayList;
+// public class program{
+//     public static void main(String[] args){
+//         Random random = new Random();
+//         int a = random.nextInt(100);
+//         int b = random.nextInt(100);
+//         int c = random.nextInt(100);
+//         int d = random.nextInt(100);
+//         int z = random.nextInt(100);
+//         ArrayList<Integer> list_1 = new ArrayList<Integer>();
+//         list_1.add(a);
+//         list_1.add(b);
+//         list_1.add(c);
+//         list_1.add(d);
+//         list_1.add(z);
+//         System.out.println(list_1);
+//         list_1.removeIf(i -> i % 2 == 0);
+//         System.out.println(list_1);
+//     }
+// }
+
+// Задан целочисленный список ArrayList. 
+// Найти минимальное, максимальное и среднее арифметическое из этого списка.
+
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Collections;
 public class program{
     public static void main(String[] args){
         Random random = new Random();
@@ -17,14 +43,13 @@ public class program{
         list_1.add(d);
         list_1.add(z);
         System.out.println(list_1);
-        for (int i = 0; i < list_1.size(); i++) {
-            if (list_1.get(i) % 2 == 0) {
-                list_1.remove(i);
-            }
+        int rez = 0;
+        for (int i = 0; i < list_1.size(); i++){
+            rez = rez + list_1.get(i);
         }
-        System.out.println(list_1);
+        rez = rez / list_1.size();
+        System.out.println(rez);
+        System.out.println(Collections.min(list_1));
+        System.out.println(Collections.max(list_1));
     }
 }
-
-// Задан целочисленный список ArrayList. 
-// Найти минимальное, максимальное и среднее арифметическое из этого списка.
